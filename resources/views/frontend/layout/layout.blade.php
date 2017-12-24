3a9b190ff93ee15d9251346d153aa16e96c951d5
mengtek <khov.mengtek92@gmail.com>
2017-12-24 15:34:59 +0700

    a

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -40,4 +40,5 @@
 
 </body>
 </html>
-<script type="text/javascript" src="{{url('js/app.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<!-- <script type="text/javascript" src="{{url('js/app.js')}}"></script> -->
