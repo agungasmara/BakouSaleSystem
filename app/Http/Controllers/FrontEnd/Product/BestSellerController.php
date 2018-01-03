4b0f0daf0392986ec09e72b70c014111bae5c578
Longdy Sim <simlongdycambodia855@gmail.com>
2018-01-03 20:49:59 +0700

    bestSeller Layout

--- app/Http/Controllers/FrontEnd/Product/BestSellerController.php
+++ app/Http/Controllers/FrontEnd/Product/BestSellerController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\FrontEnd\Product;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Carbon\Carbon;
+use DB;
 class BestSellerController extends Controller
 {
     /**
@@ -13,6 +14,32 @@ class BestSellerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(){
-    	dd("Helloworld");
+    	$Products = array();
+    	$results = $this->getBestSellerProducts(4);
+    	// dd($results);
+    }
+
+    public function getBestSellerProducts($limit){
+    	$product_data = array();
+
+		// $query = $this->db->query("SELECT op.product_id, SUM(op.quantity) AS total FROM " . DB_PREFIX . "order_product op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.order_id = o.order_id) LEFT JOIN `" . DB_PREFIX . "product` p ON (op.product_id = p.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) WHERE o.order_status_id > '0' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' GROUP BY op.product_id ORDER BY total DESC LIMIT " . (int)$limit);
+		$query = DB::table('order_product')
+				->leftJoin('order','order.order_id','=','order.order_id')
+				->leftJoin('product','order_product.product_id','=','order.product_id')
+				->leftJoin('product_to_store','product.product_id','=','product_to_store.product_id')
+				
+				->limit(4)
+				->get();
+		dd($query);
+			
+
+
+		foreach ($query->rows as $result) {
+			$product_data[$result['product_id']] = $this->getProduct($result['product_id']);
+		}
+
+		$this->cache->set('product.bestseller.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit, $product_data);
+
+		return $product_data;
     }
 }
