8b1a6261f6a542d8d26b8c31ef17d8f0418f94ff
Sineth Sim <simsineth855@gmail.com>
2018-01-13 21:03:55 +0700

    Check App

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -305,17 +305,6 @@ const router = new VueRouter({
 //   ]
 // })
 
-// app frontend
-// if(document.getElementById("#root_frontend") ) {
-  const appf = new Vue({
-      // el: '#root',
-      router,
-      template: `
-      <appf></appf>`,
-      components: { Appf },
-  }).$mount('#root_frontend')
-// }
-
 // if( document.getElementById("#root") ) {
   // app backend
   const app = new Vue({
@@ -328,6 +317,17 @@ const router = new VueRouter({
   }).$mount('#root')
 // }
 
+// app frontend
+// if(document.getElementById("#root_frontend") ) {
+  const appf = new Vue({
+      // el: '#root',
+      router,
+      template: `
+      <appf></appf>`,
+      components: { Appf },
+  }).$mount('#root_frontend')
+// }
+
 // if( document.getElementById("#root_login") ) {
   const AuthLogin = new Vue({
       // el: '#root',
@@ -338,48 +338,3 @@ const router = new VueRouter({
       components: { Appl },
   }).$mount('#root_login')
 // }
-
-
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-// import routes from './router/router'
-
-// import Home from './views/Components/index.vue'
-// import Register from './views/Auth/Register.vue'
-// import Login from './views/Auth/Login.vue'
-// import List from './views/Auth/List.vue'
-// import ShowForm from './views/Auth/Show.vue'
-// import CreateForm from './views/Auth/Form.vue'
-
-// // import store from './store/'
-// import {routerMode} from './config/env'
-// import './config/rem'
-// import FastClick from 'fastclick'
-
-// if ('addEventListener' in document) {
-//     document.addEventListener('DOMContentLoaded', function() {
-//         FastClick.attach(document.body);
-//     }, false);
-// }
-
-// Vue.use(VueRouter)
-// const router = new VueRouter({
-//   routes,
-//   mode: routerMode,
-//   strict: process.env.NODE_ENV !== 'production',
-//   scrollBehavior (to, from, savedPosition) {
-//       if (savedPosition) {
-//         return savedPosition
-//     } else {
-//       if (from.meta.keepAlive) {
-//         from.meta.savedPosition = document.body.scrollTop;
-//       }
-//         return { x: 0, y: to.meta.savedPosition || 0 }
-//     }
-//   }
-// })
-
-
-// const app = new Vue({
-//   router,
-// }).$mount('#root')
