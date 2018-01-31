8815ac8148338c5e3cbdd7231eeb67bd2a4c8bd9
Sineth Sim <simsineth855@gmail.com>
2018-01-31 20:27:29 +0700

    Wishlist and Loading Progress

--- routes/web.php
+++ routes/web.php
@@ -133,6 +133,10 @@ Route::resource('/', 'FrontEnd\Product\CartController');
 Route::get('my_account', 'FrontEnd\Product\CartController@index');
 Route::get('signin', 'FrontEnd\Product\CartController@index');
 
+// Account Customer
+Route::get('/api/address', 'FrontEnd\Account\AddressController@index');
+Route::get('/api/wishlist', 'FrontEnd\Account\WishlistController@index');
+
 // Front End API
 Route::get('/api/getTest', 'FrontEnd\TestController@getTest');
 Route::get('/api/show/{id}', 'FrontEnd\TestController@getShow');
