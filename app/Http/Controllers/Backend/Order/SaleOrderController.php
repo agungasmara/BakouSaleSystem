dc07c8bceb819c82c6375fece86c455ac8d900bb
mengtek <khov.mengtek92@gmail.com>
2018-03-11 03:40:13 +0700

    sale order

--- app/Http/Controllers/Backend/Order/SaleOrderController.php
+++ app/Http/Controllers/Backend/Order/SaleOrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Models\FrontEnd\Order\OrderModel;
+use App\Http\Models\FrontEnd\Order\OrderProduct;
+use App\Http\Models\BackEnd\CustomerGroup\CustomerGroupDescription\CustomerGroupDescription;
 
 class SaleOrderController extends Controller
 {
@@ -58,7 +60,13 @@ class SaleOrderController extends Controller
      */
     public function edit($id)
     {
-        //
+        $data=OrderModel::find($id);
+        $data->customer_group_name=CustomerGroupDescription::find($data->customer_group_id)->value('name');
+        $result['data']=$data;
+        $result['products']=OrderProduct::where('order_id',$data->order_id)->get();
+        $result['sub_total']=OrderProduct::where('order_id',$data->order_id)->sum('total');
+        $result['total']=OrderProduct::where('order_id',$data->order_id)->sum('total');
+        return $result;
     }
 
     /**
