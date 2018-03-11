323637d216eb19a0a7a1fdb88ad030644628d039
Longdy Sim <simlongdycambodia855@gmail.com>
2018-03-11 22:22:38 +0700

    - Quick view

--- resources/assets/js/views/Components/frontend/product/_recommended.vue
+++ resources/assets/js/views/Components/frontend/product/_recommended.vue
@@ -1,4 +1,5 @@
 <template>
+
 	<div v-bind:class="{ active: isActive }" class="loading">
 		<div class="morePost row featuredPostContainer style2 globalPaddingTop ">
 		      <h3 class="section-title style2 text-center"><span>RECOMMADED PRODUCT</span></h3>
@@ -15,12 +16,13 @@
 
 		                      <div class="image">
 		                          <div class="quickview">
-		                              <a data-toggle="modal" class="btn btn-xs btn-quickview" href="ajax/product"
-		                                 data-target="#productSetailsModalAjax">Quick View </a>
+		                            <a data-toggle="modal" class="btn btn-xs btn-quickview" @click="AddToCart(productRecommanded.product_id)"
+		                                 data-target="#productSetailsModalAjax" :id-product="productRecommanded.product_id">Quick View 
+		                            </a>
 		                          </div>
 		                          <router-link v-bind:to="'/product/product_detail/'+ productRecommanded.product_id">
 		                          	 <img v-bind:src="productRecommanded.thumb" alt="img" class="img-responsive">
-									</router-link>
+								  </router-link>
 
 		                          <div class="promotion">
 		                          	<span class="new-product"> NEW</span>
@@ -36,13 +38,16 @@
 
 		                          <p v-html="productRecommanded.description">.</p>
 		                          <span class="size">XL / XXL / S </span></div>
+
 		                      <div class="price" v-if="productRecommanded.special != ''">
 		                      	<span>  ${{productRecommanded.price}}</span> 
 		                      </div>
+
 		                      <div class="price" v-else>
 		                      	<span>  ${{productRecommanded.price}}</span> 
 		                      	<span class="old-price"> ${{productRecommanded.special}}</span>
 		                      </div>
+
 		                      <div class="action-control">
 		                      	<a class="btn btn-primary" @click="AddToCart(productRecommanded.product_id)"> 
 		                          <span class="add2cart">
@@ -62,8 +67,149 @@
 		          </div>
 		      </div>
 		      <!--/.container-->
+		      	<!-- Product Details Modal  -->
+				<!-- Modal -->
+				<div class="modal fade" id="productSetailsModalAjax" tabindex="-1" role="dialog"
+				 aria-labelledby="productSetailsModalAjaxLabel" aria-hidden="true">
+				<div class="modal-dialog">
+				    <div class="modal-content" v-for="productRecommanded of productRecommandeds">
+				        <button aria-hidden="true" data-dismiss="modal" class="close" type="button"> ×</button>
+				        <div class="col-lg-5 col-md-5 col-sm-5  col-xs-12">
+				            <!-- product Image -->
+				            <div class="main-image  col-lg-12 no-padding style3">
+				                <a class="product-largeimg-link" href="">
+				                	<img src="images/product_details/low-res-white/1.jpg" class="img-responsive product-largeimg" alt="img">
+				                </a>
+				            </div>
+				            <!--/.main-image-->
+
+				            <div class="modal-product-thumb">
+				                <a class="thumbLink selected"><img data-large="images/product_details/low-res-white/1.jpg" alt="img"
+				                                                   class="img-responsive" src="images/product_details/low-res-white/1.jpg">
+				                </a>
+				                <a class="thumbLink"><img data-large="images/product_details/low-res-white/2.jpg" alt="img"
+				                                          class="img-responsive"
+				                                          src="images/product_details/low-res-white/2.jpg">
+				                </a>
+				                <a class="thumbLink"><img data-large="images/product_details/low-res-white/3.jpg" alt="img"
+				                                          class="img-responsive"
+				                                          src="images/product_details/low-res-white/3.jpg">
+				                </a>
+				            </div>
+				            <!--/.modal-product-thumb-->
+				        </div>
+				        <!--/ product Image-->
+
+				        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 modal-details no-padding">
+				            <div class="modal-details-inner">
+				                <h1 class="product-title"> Lorem ipsum dolor sit amet</h1>
+
+				                    <h3 class="product-code">Product Code : DEN1098</h3>
+
+				                    <div class="product-price"><span class="price-sales"> $70</span> <span
+				                            class="price-standard">$95</span></div>
+				                    <div class="details-description">
+				                        <p>In scelerisque libero ut elit porttitor commodo Suspendisse laoreet magna. </p>
+				                    </div>
+				                    <div class="color-details"><span class="selected-color"><strong>COLOR</strong></span>
+				                        <ul class="swatches Color">
+				                            <li class="selected">
+				                                <a style="background-color:#f1f40e"> </a>
+				                            </li>
+				                            <li>
+				                                <a style="background-color:#adadad"> </a>
+				                            </li>
+				                            <li>
+				                                <a style="background-color:#4EC67F"> </a>
+				                            </li>
+				                        </ul>
+				                    </div>
+				                    <!--/.color-details-->
+
+				                    <div class="productFilter productFilterLook2">
+				                        <div class="row">
+				                            <div class="col-lg-6 col-sm-6 col-xs-6">
+				                                <div class="filterBox">
+				                                    <select class="form-control">
+				                                        <option value="strawberries" selected>Quantity</option>
+				                                        <option value="mango">1</option>
+				                                        <option value="bananas">2</option>
+				                                        <option value="watermelon">3</option>
+				                                        <option value="grapes">4</option>
+				                                        <option value="oranges">5</option>
+				                                        <option value="pineapple">6</option>
+				                                        <option value="peaches">7</option>
+				                                        <option value="cherries">8</option>
+				                                    </select>
+				                                </div>
+				                            </div>
+				                            <div class="col-lg-6 col-sm-6 col-xs-6">
+				                                <div class="filterBox">
+				                                    <select class="form-control">
+				                                        <option value="strawberries" selected>Size</option>
+				                                        <option value="mango">XL</option>
+				                                        <option value="bananas">XXL</option>
+				                                        <option value="watermelon">M</option>
+				                                        <option value="apples">L</option>
+				                                        <option value="apples">S</option>
+				                                    </select>
+				                                </div>
+				                            </div>
+				                        </div>
+				                    </div>
+				                    <!-- productFilter -->
+
+				                    <div class="cart-actions">
+				                        <div class="addto row">
+				                            <div class="col-lg-6">
+				                                <button onclick="productAddToCartForm.submit(this);"
+				                                        class="button btn-block btn-cart cart first" title="Add to Cart"
+				                                        type="button">
+				                                    Add to Cart
+				                                </button>
+				                            </div>
+				                            <div class="col-lg-6"><a class="link-wishlist wishlist btn-block ">Add to Wishlist</a>
+				                            </div>
+				                        </div>
+				                    </div>
+				                    <!--/.cart-actions-->
+
+				                    <div class="product-share clearfix">
+				                        <p> SHARE </p>
+
+				                        <div class="socialIcon">
+				                            <a href="#"> <i class="fa fa-facebook"></i>
+				                            </a>
+				                            <a href="#"> <i class="fa fa-twitter"></i>
+				                            </a>
+				                            <a href="#"> <i class="fa fa-google-plus"></i>
+				                            </a>
+				                            <a href="#"> <i class="fa fa-pinterest"></i>
+				                            </a>
+				                        </div>
+				                    </div>
+				                    <!--/.product-share-->
+				            </div>
+				            <!--/.modal-details-inner-->
+				        </div>
+				        <!--/.modal-details-->
+				        <div class="clear"></div>
+
+				    </div>
+				    <!-- /.modal-content -->
+				</div>
+				<!-- /.modal-dialog -->
+				</div>
+				<!-- /.modal -->
+				<!-- End Modal -->
 		</div>
 	</div>
+	<!-- Quick View -->
+	<!-- Product Details Modal  -->
+
+<!-- End Modal -->
+
+<!-- End Modal -->
 </template>
 
 <script type="text/javascript">
