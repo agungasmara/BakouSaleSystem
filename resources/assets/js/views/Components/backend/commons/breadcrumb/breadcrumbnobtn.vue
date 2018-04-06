a36211228145d90488f292cfddb7929959d23b27
snak-evil <sovotanakpath579@gmail.com>
2018-04-06 19:43:30 +0700

    reseller

---
+++ resources/assets/js/views/Components/backend/commons/breadcrumb/breadcrumbnobtn.vue
@@ -0,0 +1,31 @@
+<template>
+	<!--breadcrumbs start-->
+		<div id="breadcrumbs-wrapper">
+			<!-- Search for small screen -->
+			<div class="header-search-wrapper grey lighten-2 hide-on-large-only">
+			  <input type="text" class="header-search-input z-depth-2" placeholder="Explore Materialize">
+			</div>
+			<div class="container">
+			  <div class="row">
+			    <div class="col s10 m6 l6">
+			      <h5 class="breadcrumbs-title">{{ breadcrumbTitle }}</h5>
+			      	<v-breadcrumbs>
+			        	<v-icon slot="divider">/</v-icon>
+		        		<v-breadcrumbs-item  v-for="item in breadcrumbItem" :key="item.text" :disabled="item.disabled">
+		          			{{ item.text }}
+		        		</v-breadcrumbs-item>
+		      		</v-breadcrumbs>
+			    </div>
+			  </div>
+			</div>
+		</div>
+		<!--breadcrumbs end-->
+</template>
+<script type="text/javascript">
+	export default{
+		props:[
+			'breadcrumbItem',
+			'breadcrumbTitle'
+		]
+	}
+</script>
